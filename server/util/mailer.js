8809a4908e9a0ed09e0fceda898c7f7d4d3e99f5
HuzaifaQayyum <huzaifaacf@gmail.com>
2022-07-24 07:22:44 -0700

    mailer modified to use env

--- server/util/mailer.js
+++ server/util/mailer.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer');
 const sgtransport = require('nodemailer-sendgrid-transport');
 
-const mailer = nodemailer.createTransport(sgtransport({
-    auth: {
-        api_key: process.env.nodemailerApiKey
+
+const mailer = nodemailer.createTransport({
+    host: process.env.mailerHost,
+    port: process.env.mailerPort,
+    secure: process.env.mailerSecure || false,
+    auth: { 
+        user: process.env.mailerUser,
+        pass: process.env.mailerPass
     }
-}));
+});
 
 module.exports = mailer;
